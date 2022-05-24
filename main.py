7019ca6d7714d454faa59638c2ae11d13a2fdc83
Justyna <j.matejkowska@gmail.com>
2022-05-24 21:04:52 +0200

    test niepełny

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ class Library:
         self.tytuly=[]       
 
     def addKsiazka(self, ksiazka): 
-        self.ksiazki.append(ksiazka)#wrzuca książkę do listy
+        self.ksiazki.append(ksiazka)
 
-    def addEgzemplarz(self,egzemplarz): #dodanie egzamplarza bądz nowej ksiązki
+    def addEgzemplarz(self,egzemplarz): 
         egzemplarz1 = list(egzemplarz)
-        if egzemplarz1[1:3] in self.ksiazki:
-            self.egzemplarze.append(egzemplarz1[1:3])
-            self.tytuly.append(egzemplarz1[1])  
+        if egzemplarz1[0:2] in self.ksiazki:
+            self.egzemplarze.append(egzemplarz1[0:2])
+            self.tytuly.append(egzemplarz1[0])  
         else:
-            library.addKsiazka(egzemplarz1[1:3])
-            self.egzemplarze.append(egzemplarz1[1:3])
-            self.tytuly.append(egzemplarz1[1])
+            library.addKsiazka(egzemplarz1[0:2])
+            self.egzemplarze.append(egzemplarz1[0:2])
+            self.tytuly.append(egzemplarz1[0])
                  
     def liczenie(self):
         listafinal=[]
@@ -25,9 +25,9 @@ class Library:
             ksiazka = [ksiazka[0], ksiazka[1], a]
         listafinal.append(ksiazka)
         listasort = sorted(listafinal, key=lambda x: x[0])
-        for element in listasort:
-            element1 = (element[0], element[1], element[2])
-        print(element1)
+        for element in listasort: 
+            element = (element[0], element[1], element[2])
+            print(element)
 
 library=Library()
 k = int(input())
